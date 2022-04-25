3062a8855a2c758b745b431ff798247bf56e77fc
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-04-25 23:04:53 +0530

    routing init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Vaahan
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<h1>Auth Page</h1>} />
+          <Route path="/user" element={<h1>User Page</h1>} />
+          <Route path="/fuelPrices" element={<h1>Fuel Page</h1>} />
+          <Route path="/my-vehicle" element={<h1>Veh Page</h1>} />
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="*" element={<h1>page not found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
