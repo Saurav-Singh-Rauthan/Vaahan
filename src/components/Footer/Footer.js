5c33e67892f8ac97276f6863eea4307c3882ff1a
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-04-27 21:23:58 +0530

    footer init

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
 import Styles from "./Footer.module.css";
 
 const Footer = (props) => {
-  return(
+  return (
     <div className={Styles.container}>
-      Footer here
+      <div>
+        © 2022{" "}
+        <a
+          className={Styles.link}
+          href="https://github.com/Saurav-Singh-Rauthan"
+          target={"_"}
+        >
+          Saurav Singh Rauthan
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
