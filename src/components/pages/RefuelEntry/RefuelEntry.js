44f5983caabe9a2e5ac0d18d4661d6c9782e6792
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-07-18 14:43:43 +0530

    ui fixes for add record page

--- src/components/pages/RefuelEntry/RefuelEntry.js
+++ src/components/pages/RefuelEntry/RefuelEntry.js
@@ -13,12 +13,16 @@ import "./customTab.css";
 const RefuelEntry = (props) => {
   const vehicles = ["Activa 4g", "TVS Victor GL", "R15 v4"];
   const [value, setValue] = useState("1");
+  const [record, setrecord] = useState({
+    vehicle: null,
+    odometerReading: null,
+    fuelAdded: null,
+  });
+  const [addNewVeh, setaddNewVeh] = useState(null);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  const [addNewVeh, setaddNewVeh] = useState(null);
 
   const newVehIpHandler = (event, value, reason) => {
     if (reason === "clear" || event.target.value.length === 0) {
@@ -26,9 +30,118 @@ const RefuelEntry = (props) => {
     } else if (reason === "selectOption") {
       setaddNewVeh(event.target.outerText);
     } else {
-      setaddNewVeh(event.target.value)
+      setaddNewVeh(event.target.value);
     }
   };
+
+  const newRecordHandler = (event, value, reason, type) => {
+    let recordToBeAdded = { ...record };
+    if (type === "odo") {
+      recordToBeAdded.odometerReading =
+        event.target.value.length === 0 ? null : event.target.value;
+      setrecord(recordToBeAdded);
+    } else if (type === "fuel") {
+      recordToBeAdded.fuelAdded =
+        event.target.value.length === 0 ? null : event.target.value;
+      setrecord(recordToBeAdded);
+    } else if (type === "veh") {
+      if (reason === "clear" || event.target.value.length === 0) {
+        recordToBeAdded.vehicle = null;
+        setrecord(recordToBeAdded);
+      } else if (reason === "selectOption") {
+        recordToBeAdded.vehicle = event.target.outerText;
+        setrecord(recordToBeAdded);
+      }
+    }
+  };
+
+  const addRecord = (
+    <div className={Styles.addRecordCont}>
+      <div className={Styles.optionsCont}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={vehicles}
+          className={Styles.options}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Select vehicle"
+              helperText="Vehicle for which record is added"
+            />
+          )}
+          onChange={(event, value, reason) =>
+            newRecordHandler(event, value, reason, "veh")
+          }
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Odometer Reading"
+          variant="outlined"
+          helperText="Odometer reading after refuel (Kms)"
+          className={Styles.options}
+          onChange={(event) => newRecordHandler(event, null, null, "odo")}
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Fuel Filled"
+          helperText="Fuel filled (Litres)"
+          variant="outlined"
+          sx={{ borderColor: "red !important" }}
+          className={Styles.options}
+          onChange={(event) => newRecordHandler(event, null, null, "fuel")}
+        />
+      </div>
+      <div className={Styles.addRecLastRec}>
+        <p>Last Refuel Details</p>
+        <div className={Styles.details}>
+          <p>
+            Odometer Reading : <strong>12000 km</strong>
+          </p>
+          <p>
+            Fuel Filled: <strong>2.01 Litre</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const addVehicle = (
+    <div style={{ position: "relative", margin: "1rem" }}>
+      <div className={Styles.optionsCont}>
+        <Autocomplete
+          onChange={(event, value, reason) =>
+            newVehIpHandler(event, value, reason)
+          }
+          freeSolo={true}
+          disablePortal
+          id="combo-box-demo"
+          options={vehicles}
+          className={Styles.options}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              onChange={(event) => newVehIpHandler(event)}
+              label="Select vehicle"
+              helperText="Select new vehicle to be added"
+            />
+          )}
+        />
+      </div>
+      <div className={Styles.vehAddSumm}>
+        Vehicle to be added is : {addNewVeh}
+      </div>
+      <button
+        className={Styles.addVehBtn}
+        disabled={addNewVeh === null}
+        onClick={() => alert("hello world")}
+      >
+        Add Vehicle
+      </button>
+    </div>
+  );
 
   return (
     <div className={Styles.container}>
@@ -46,104 +159,49 @@ const RefuelEntry = (props) => {
               <Tab label="Add Vehicle" value="2" />
             </TabList>
           </Box>
-          <TabPanel value="1">
-            <div className={Styles.addRecordCont}>
-              <div className={Styles.optionsCont}>
-                <Autocomplete
-                  disablePortal
-                  id="combo-box-demo"
-                  options={vehicles}
-                  className={Styles.options}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Select vehicle"
-                      helperText="Vehicle for which record is added"
-                    />
-                  )}
-                />
-
-                <TextField
-                  id="outlined-basic"
-                  label="Odometer Reading"
-                  variant="outlined"
-                  helperText="Odometer reading after refuel (Kms)"
-                  className={Styles.options}
-                />
-
-                <TextField
-                  id="outlined-basic"
-                  label="Fuel Filled"
-                  helperText="Fuel filled (Litres)"
-                  variant="outlined"
-                  sx={{ borderColor: "red !important" }}
-                  className={Styles.options}
-                />
-              </div>
-              <div className={Styles.addRecLastRec}>
-                <p>Last Refuel Details</p>
-                <div className={Styles.details}>
-                  <p>
-                    Odometer Reading : <strong>12000 km</strong>
-                  </p>
-                  <p>
-                    Fuel Filled: <strong>2.01 Litre</strong>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </TabPanel>
-          <TabPanel value="2">
-            <div
-              style={{ position: "relative", margin: "1rem" }}
-            >
-              <div className={Styles.optionsCont}>
-                <Autocomplete
-                  onChange={(event, value, reason) =>
-                    newVehIpHandler(event, value, reason)
-                  }
-                  freeSolo={true}
-                  disablePortal
-                  id="combo-box-demo"
-                  options={vehicles}
-                  className={Styles.options}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      onChange={(event) => newVehIpHandler(event)}
-                      label="Select vehicle"
-                      helperText="Select new vehicle to be added"
-                    />
-                  )}
-                />
-              </div>
-              <div className={Styles.vehAddSumm}>
-                Vehicle to be added is : {addNewVeh}
-              </div>
-              <button
-                className={Styles.addVehBtn}
-                disabled={addNewVeh === null}
-                onClick={() => alert("hello world")}
-              >
-                Add Vehicle
-              </button>
-            </div>
-          </TabPanel>
+          <TabPanel value="1">{addRecord}</TabPanel>
+          <TabPanel value="2">{addVehicle}</TabPanel>
         </TabContext>
       </div>
-      <div className={Styles.reDiv}>
+      <div className={Styles.reDiv} style={{display : value == 1 ? "block" : "none"}}>
         <div className={Styles.summTitle}>Summary</div>
-        <div className={Styles.summCont}>
-          <div className={Styles.summDetails}>
-            <p>12000 KM</p>
-            <span>Odometer Reading</span>
+        {record.vehicle !== null &&
+        record.fuelAdded !== null &&
+        record.odometerReading !== null ? (
+          <div className={Styles.summCont}>
+            <div className={Styles.summDetails}>
+              <p>{record.odometerReading} KM</p>
+              <span>Odometer Reading</span>
+            </div>
+            <div className={Styles.summDetails}>
+              <p>{record.fuelAdded} Litres</p>
+              <span>Fuel Filled</span>
+            </div>
+            <p className={Styles.summVehType}>{record.vehicle}</p>
           </div>
-          <div className={Styles.summDetails}>
-            <p>2.01 Litres</p>
-            <span>Fuel Filled</span>
-          </div>
-        </div>
-        <button>Submit</button>
+        ) : (
+          <p
+            style={{
+              justifyContent: "center",
+              display: "flex",
+              alignItems: "center",
+              height: "50%",
+            }}
+          >
+            Add details to view summary....
+          </p>
+        )}
+        <button
+          disabled={
+            record.vehicle !== null &&
+            record.fuelAdded !== null &&
+            record.odometerReading !== null
+              ? false
+              : true
+          }
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
